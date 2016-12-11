b11e387654f9e8ee52a291b8f7c35b0c1ef0a34b
Patrick Nikić <rickpa007@hotmail.com>
2016-12-11 23:30:55 +0100

    Laser sculptures

---
+++ Hacking/uva/11683.py
@@ -0,0 +1,19 @@
+while True:
+  dimIn = input().split() #Prva linija inputa pretvorena u listu stringova
+  dim = [int(elem) for elem in dimIn]
+  if dim[0] == 0:
+    break
+  
+  L = input().split()
+  L = [int(elem) for elem in L]
+  
+  laser = dim[0] - L[0]
+  
+  for jj in range(1, dim[1]):
+    diff = L[jj - 1] - L[jj]
+    if diff > 0:
+        laser += diff
+  
+  print(laser)
+
+continue
