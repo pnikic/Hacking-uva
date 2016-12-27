06e6a258f414dd9691a145c768456219b37e6b62
Patrick Nikić <rickpa007@hotmail.com>
2016-12-27 12:01:17 +0100

    Updates: Zeros and Ones

--- Hacking/uva/10324.cpp
+++ Hacking/uva/10324.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <utility>
+//#include <algorithm> std::swap() (until C++11)
 #include <string>
 #include <cstring>
 using namespace std;
@@ -6,11 +8,7 @@ using namespace std;
 void MinAdjust(int &a, int&b)
 {
 	if (b < a)
-	{
-		int tmp = b;
-		b = a;
-		a = tmp;
-	}
+		swap(a,b)
 }
 
 int main()
@@ -19,11 +17,10 @@ int main()
 	int A[1000000], cnt = 1;
 	while (getline(cin, s))
 	{
-		memset(A, 0, 1000000 * sizeof(int));
 		//Cumulative sum
-		A[0] += s[0] - 48;
-		for (int i = 0; i < s.size(); ++i)
-			A[i] += s[i] - 48 + A[i - 1];
+		A[0] += s[0] - '0';
+		for (int i = 1; i < s.size(); ++i)
+			A[i] += s[i] - '0' + A[i - 1];
 			
 		cout << "Case " << cnt++ << ":\n";
 		int q;
