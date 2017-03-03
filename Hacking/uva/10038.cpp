6d281f8dfaa0b5cd76228f1118ffbf2a2ba905c4
Patrick Nikić <rickpa007@hotmail.com>
2017-03-03 22:27:37 +0100

    Jolly Jumpers

---
+++ Hacking/uva/10038.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int abs(int a)
+{
+	return a > 0 ? a : -a;
+}
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		bool A[3000];
+		memset(A, false, 3000*sizeof(bool));
+		
+		int cnt = 0;		
+		int a, b;
+		cin >> a;
+		for(int i = 1; i < n; ++i)
+		{
+			cin >> b;
+			int ind = abs(a-b) - 1;
+			if (!A[ind] && ind < n - 1)
+				A[ind] = true, cnt++;
+			a = b;
+		}
+				
+		if (cnt == n - 1)
+			cout << "Jolly\n";
+		else
+			cout << "Not jolly\n";	
+	}	
+}
